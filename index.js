0c259936868308c62a8ade5b450e0ef9cf74b21f
Dear Bear <dearerbear@gmail.com>
2018-08-08 21:33:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function outerFunction() {
   var innerVariable = "I'm sort of a secret.";
- 
+
   return function innerScope() {
     var inaccessible = "Nothing can touch me.";
- 
-    return innerVariable;
+
+    console.log(innerVariable);
   }
 }
